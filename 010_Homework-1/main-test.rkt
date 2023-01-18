638d4fe8e5a09260348c45670ba5825b58c6d99d
dharmikpatel <dharmik529@gmail.com>
2023-01-18 14:39:18 -0500

    finished negate-tree

--- 010_Homework-1/main-test.rkt
+++ 010_Homework-1/main-test.rkt
@@ -8,11 +8,17 @@
                               (node 3 (node 4 empty-tree empty-tree)
                                             empty-tree)))
 
-(define neg-tree-one (node -3 empty-tree empty-tree))
+(define neg-tree-one (node -1 (leaf -2) (leaf -3)))
 (define neg-tree-two (node -1 (node -2 empty-tree empty-tree)
                               (node -3 (node -4 empty-tree empty-tree)
                                             empty-tree)))
 
-(test (tree-sum tree-one) 6)    
+;;; (test (tree-sum empty-tree) 0)
+;;; (test (tree-sum tree-one) 6)
+;;; (test (tree-sum tree-two) 10)
+
+(test (negate-tree tree-one) neg-tree-one)
+(test (negate-tree tree-two) neg-tree-two)
+
   
 
--- 010_Homework-1/main.rkt
+++ 010_Homework-1/main.rkt
@@ -19,7 +19,9 @@
 
 ;;; Implement the function negate, which takes a tree and returns 
 ;;; a tree that has the same shape, but with all the numbers negated.
-;;; (define (negate-tree atree output-tree)
-;;;   (cond
-;;;   [ (leaf? atree) 0]
-;;;   [ (node? atree) ( )]))
+(define (negate-tree atree)
+  (cond [ (leaf? atree) (leaf (* (leaf-val atree) -1))]
+        [ (node? atree) (node (* (node-val atree) -1) (negate-tree (node-left atree)) (negate-tree (node-right atree)))]))
+
+;;; Implement the function contains?, which takes a tree and a number 
+;;; and returns #t if the number is in the tree, #f otherwise.
