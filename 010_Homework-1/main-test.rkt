49e8495736bed60fd922c6eed70bc72d473a8adf
dharmikpatel <dharmik529@gmail.com>
2023-01-17 23:17:32 -0500

    created test case for negate-tree

--- 010_Homework-1/main-test.rkt
+++ 010_Homework-1/main-test.rkt
@@ -3,11 +3,30 @@
 (require rackunit "main.rkt")
 (require rackunit/text-ui)
 
-(define tree-sum-test
+(define empty-tree (make-leaf))
+(define tree-one (make-tree 3 empty-tree empty-tree))
+(define tree-two (make-tree 1 (make-tree 2 empty-tree empty-tree)
+                              (make-tree 3 (make-tree 4 empty-tree empty-tree)
+                                            empty-tree)))
+
+(define neg-tree-one (make-tree -3 empty-tree empty-tree))
+(define neg-tree-two (make-tree -1 (make-tree -2 empty-tree empty-tree)
+                              (make-tree -3 (make-tree -4 empty-tree empty-tree)
+                                            empty-tree)))
+
+(define main-test
   (test-suite
-    "Test for tree-sum"
-    (check-eq?(tree-sum empty-tree) 0 "Not equal")
-    (check-eq?(tree-sum tree-one) 3 "Not equal")
-    (check-eq?(tree-sum tree-two) 10 "Not equal")))
+  "Test for main-test"
+    (test-case
+      "Test for tree-sum"
+      (check-eq?(tree-sum empty-tree) 0 "Not equal")
+      (check-eq?(tree-sum tree-one) 3 "Not equal")
+      (check-eq?(tree-sum tree-two) 10 "Not equal"))
+    (test-case
+      "Test for tree-sum"
+      (check-eq?(negate-tree empty-tree) 0 "Not negated")
+      (check-eq?(negate-tree tree-one) neg-tree-one "Not negated")
+      (check-eq?(negate-tree tree-two) neg-tree-two "Not negated"))  
+    ))
 
 (run-tests tree-sum-test)
--- 010_Homework-1/main.rkt
+++ 010_Homework-1/main.rkt
@@ -3,11 +3,7 @@
 (define-struct leaf [])
 (define-struct tree (num left right))
 
-(define empty-tree (make-leaf))
-(define tree-one (make-tree 3 empty-tree empty-tree))
-(define tree-two (make-tree 1 (make-tree 2 empty-tree empty-tree)
-                              (make-tree 3 (make-tree 4 empty-tree empty-tree)
-                                            empty-tree)))
+
 
 (define (tree-sum atree)
   (cond
@@ -16,6 +12,11 @@
                      (tree-sum (tree-left atree))
                      (tree-sum (tree-right atree)))]))
 
-(provide tree-sum empty-tree tree-one tree-two)
+(define (negate-tree atree output-tree)
+  (cond
+  [ (leaf? atree) 0]
+  [ (tree? atree) ( )]))
+
+(provide tree-sum negate-tree)
 
 ;;; https://www.cs.unb.ca/~bremner/teaching/cs4613/racket/plait-demo.rkt/
