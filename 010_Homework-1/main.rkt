513677589916080debde8f90f5602f71d18f4cc5
dharmikpatel <dharmik529@gmail.com>
2023-01-17 23:19:12 -0500

    created test case for negate-tree

--- 010_Homework-1/main.rkt
+++ 010_Homework-1/main.rkt
@@ -4,7 +4,8 @@
 (define-struct tree (num left right))
 
 
-
+;;; Implement a sum function that takes a tree and returns the 
+;;; sum of the numbers in the tree. 
 (define (tree-sum atree)
   (cond
   [ (leaf? atree) 0]
@@ -12,6 +13,8 @@
                      (tree-sum (tree-left atree))
                      (tree-sum (tree-right atree)))]))
 
+;;; Implement the function negate, which takes a tree and returns 
+;;; a tree that has the same shape, but with all the numbers negated.
 (define (negate-tree atree output-tree)
   (cond
   [ (leaf? atree) 0]
