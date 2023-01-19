cbfa33ca266c14a847fa634f268c82216b04869a
dharmikpatel <dharmik529@gmail.com>
2023-01-18 21:32:26 -0500

    completed bigleaves and positivetrees

--- 010_Homework-1/main-test.rkt
+++ 010_Homework-1/main-test.rkt
@@ -4,15 +4,15 @@
 (define empty-tree (leaf 0))
 (define tree-one (node 1 (leaf 2) (leaf 3)))
 (define tree-two (node 1 (node 2 empty-tree empty-tree)
-                              (node 3 (node 4 empty-tree empty-tree)
-                                            empty-tree)))
+                       (node 3 (node 4 empty-tree empty-tree)
+                             empty-tree)))
 
 (define neg-tree-one (node -1 (leaf -2) (leaf -3)))
 (define neg-tree-two (node -1 (node -2 empty-tree empty-tree)
-                              (node -3 (node -4 empty-tree empty-tree)
-                                            empty-tree)))
+                           (node -3 (node -4 empty-tree empty-tree)
+                                 empty-tree)))
 
-;;;Test for Q1
+;;; Test for Q1
 (test (tree-sum empty-tree) 0)
 (test (tree-sum tree-one) 6)
 (test (tree-sum tree-two) 10)
@@ -21,12 +21,35 @@
 (test (negate-tree tree-one) neg-tree-one)
 (test (negate-tree tree-two) neg-tree-two)
 
-;;;Test for Q3
+;;; Test for Q3
 (test (contains? tree-one 6) #f)
 (test (contains? tree-one 3) #t)
 (test (contains? tree-two 1) #t)
 (test (contains? tree-two 9) #f)
 
+;;; Test for Q4
+(test (big-leaves? (node 5 (leaf 6) (leaf 7))) #t)
+(test (big-leaves? (node 5 (node 2 (leaf 8) (leaf 6)) (leaf 7))) #f)
 
-  
+;;; Test for Q5
+(test (positive-trees? (cons (leaf 6)
+                             empty))
+      #t)
 
+(test (positive-trees? (cons (leaf -6)
+                             empty))
+      #f)
+
+(test (positive-trees? (cons (node 1 (leaf 6) (leaf -6))
+                             empty))
+      #t)
+
+(test (positive-trees? (cons (node 1 (leaf 6) (leaf -6))
+                             (cons (node 0 (leaf 0) (leaf 1))
+                                   empty)))
+      #t)
+
+(test (positive-trees? (cons (node -1 (leaf 6) (leaf -6))
+                             (cons (node 0 (leaf 0) (leaf 1))
+                                   empty)))
+        #f)
--- 010_Homework-1/main.rkt
+++ 010_Homework-1/main.rkt
@@ -3,29 +3,49 @@
 ;;; (define-struct leaf [])
 ;;; (define-struct tree (num left right))
 
-(define-type Tree 
-    (leaf [val : Number]) 
-    (node [val : Number] 
-          [left : Tree] 
-          [right : Tree]))
+(define-type Tree
+  (leaf [val : Number])
+  (node [val : Number]
+        [left : Tree]
+        [right : Tree]))
 
 
-;;; Implement a sum function that takes a tree and returns the 
-;;; sum of the numbers in the tree. 
+;;; Implement a sum function that takes a tree and returns the
+;;; sum of the numbers in the tree.
 (define (tree-sum atree)
   (cond
-  [ (leaf? atree) (leaf-val atree)]
-  [ (node? atree) (+(+ (node-val atree) (tree-sum (node-left atree))) (tree-sum (node-right atree)))]))
+    [ (leaf? atree) (leaf-val atree)]
+    [ (node? atree) (+(+ (node-val atree) (tree-sum (node-left atree))) (tree-sum (node-right atree)))]))
 
-;;; Implement the function negate, which takes a tree and returns 
+;;; Implement the function negate, which takes a tree and returns
 ;;; a tree that has the same shape, but with all the numbers negated.
 (define (negate-tree atree)
   (cond [ (leaf? atree) (leaf (* (leaf-val atree) -1))]
         [ (node? atree) (node (* (node-val atree) -1) (negate-tree (node-left atree)) (negate-tree (node-right atree)))]))
 
-;;; Implement the function contains?, which takes a tree and a number 
+;;; Implement the function contains?, which takes a tree and a number
 ;;; and returns #t if the number is in the tree, #f otherwise.
 (define (contains? atree anum)
   (cond [(leaf? atree) (= (leaf-val atree) anum)]
         [(node? atree) (or (= (node-val atree) anum) (contains? (node-left atree) anum) (contains? (node-right atree) anum))]
+        [else #f]))
+
+;;; Implement the function big-leaves?, which takes a tree and
+;;; returns #t if every leaf is bigger than the sum of numbers in
+;;; the path of nodes from the root that reaches the leaf.
+(define (bigger-leaves? atree currSum)
+  (cond [(leaf? atree) (> (leaf-val atree) currSum)]
+        [(node? atree) (and (bigger-leaves? (node-left atree) (+ currSum (node-val atree))) (bigger-leaves? (node-right atree) (+ currSum (node-val atree))))]
+        [else #f]))
+
+(define (big-leaves? atree)
+  (bigger-leaves? atree 0))
+
+;;; Implement the function positive-trees?, which takes a list of trees
+;;; and returns #t if every member of the list is a positive tree, where
+;;; a positive tree is one whose numbers sum to a positive value.
+(define (positive-trees? alist)
+  (cond [(empty? alist) #t]
+        [(leaf? (first alist)) (and (> (tree-sum (first alist)) 0) (positive-trees? (rest alist)))]
+        [(node? (first alist)) (and (> (tree-sum (first alist)) 0) (positive-trees? (rest alist)))]
         [else #f]))
